371c37e843ab68031a3d5f54a03bd0cf53ffb670
pula-ashok <ashokpula0307@gmail.com>
2024-09-13 10:11:30 +0530

    cart page ui completed

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,13 +1,17 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./navbar.css";
 import { assets } from "./../../assets/assets";
 import { Link } from "react-router-dom";
+import { storeContext } from "../../context/storeContext";
 
 const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("home");
+  const { getTotalCartAmount } = useContext(storeContext);
   return (
     <div className="navbar">
-      <img src={assets?.logo} alt="" className="logo" />
+      <Link to={"/"}>
+        <img src={assets?.logo} alt="" className="logo" />
+      </Link>
       <ul className="navbar-menu">
         <Link
           to={"/"}
@@ -41,8 +45,10 @@ const Navbar = ({ setShowLogin }) => {
       <div className="navbar-right">
         <img src={assets?.search_icon} alt="" />
         <div className="navbar-search-icon">
-          <img src={assets?.basket_icon} alt="" />
-          <div className="dot"></div>
+          <Link to={"/cart"}>
+            <img src={assets?.basket_icon} alt="" />
+          </Link>
+          <div className={getTotalCartAmount() === 0 ? " " : "dot"}></div>
         </div>
         <button
           onClick={() => {
--- frontend/src/context/storeContext.jsx
+++ frontend/src/context/storeContext.jsx
@@ -21,12 +21,23 @@ const StoreContextProvider = (props) => {
       return { ...prev, [itemId]: prev[itemId] - 1 };
     });
   };
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = food_list.find((product) => product._id === item);
+        totalAmount += itemInfo.price * cartItems[item];
+      }
+    }
+    return totalAmount;
+  };
   const contextValue = {
     food_list: food_list,
     cartItems,
     setCartItems,
     addToCart,
     removeFromCart,
+    getTotalCartAmount,
   };
   useEffect(() => {
     console.log(cartItems);
